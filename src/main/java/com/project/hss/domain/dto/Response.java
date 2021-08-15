d4e0898c913e0acf7f6934e9563a1ba733577b93
JianChoi-Kor <wldks79@gmail.com>
2021-08-15 22:21:35 +0900

    < Setting > - REST API Response 생성

--- src/main/java/com/project/hss/domain/dto/Response.java
+++ src/main/java/com/project/hss/domain/dto/Response.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 
 @Component
 public class Response {
@@ -15,9 +17,9 @@ public class Response {
     private static class Body {
 
         private int state;
-        private Object data;
         private String result;
         private String massage;
+        private Object data;
         private Object error;
     }
 
@@ -32,14 +34,101 @@ public class Response {
         return ResponseEntity.ok(body);
     }
 
+    /**
+     * <p> 메세지만 가진 성공 응답을 반환한다.</p>
+     * <pre>
+     *     {
+     *         "state" : 200,
+     *         "result" : success,
+     *         "message" : message,
+     *         "data" : [],
+     *         "error" : []
+     *     }
+     * </pre>
+     *
+     * @param msg 응답 바디 message 필드에 포함될 정보
+     * @return 응답 객체
+     */
     public ResponseEntity<?> success(String msg) {
         return success(Collections.emptyList(), msg, HttpStatus.OK);
     }
 
+    /**
+     * <p> 데이터만 가진 성공 응답을 반환한다.</p>
+     * <pre>
+     *     {
+     *         "state" : 200,
+     *         "result" : success,
+     *         "message" : null,
+     *         "data" : [{data1}, {data2}...],
+     *         "error" : []
+     *     }
+     * </pre>
+     *
+     * @param data 응답 바디 data 필드에 포함될 정보
+     * @return 응답 객체
+     */
     public ResponseEntity<?> success(Object data) {
         return success(data, null, HttpStatus.OK);
     }
 
+    /**
+     * <p> 성공 응답만 반환한다. </p>
+     * <pre>
+     *     {
+     *         "state" : 200,
+     *         "result" : success,
+     *         "message" : null,
+     *         "data" : [],
+     *         "error" : []
+     *     }
+     * </pre>
+     *
+     * @return 응답 객체
+     */
+    public ResponseEntity<?> success() {
+        return success(Collections.emptyList(), null, HttpStatus.OK);
+    }
 
+    public ResponseEntity<?> fail(Object data, String msg, HttpStatus status) {
+        Body body = Body.builder()
+                .state(status.value())
+                .data(data)
+                .result("fail")
+                .massage(msg)
+                .error(Collections.emptyList())
+                .build();
+        return ResponseEntity.ok(body);
+    }
 
+    /**
+     * <p> 메세지를 가진 실패 응답을 반환한다. </p>
+     * <pre>
+     *     {
+     *         "state" : HttpStatus Code,
+     *         "result" : fail,
+     *         "message" : message,
+     *         "data" : [],
+     *         "error" : [{error1}, {error2}...]
+     *     }
+     * </pre>
+     *
+     * @param msg 응답 바디 message 필드에 포함될 정보
+     * @param status 응답 바디 status 필드에 포함될 응답 상태 코드
+     * @return 응답 객체
+     */
+    public ResponseEntity<?> fail(String msg, HttpStatus status) {
+        return fail(Collections.emptyList(), msg, status);
+    }
+
+    public ResponseEntity<?> fail(LinkedList<LinkedHashMap<String, String>> error) {
+        Body body = Body.builder()
+                .state(HttpStatus.BAD_REQUEST.value())
+                .data(Collections.emptyList())
+                .result("fail")
+                .massage("")
+                .error(error)
+                .build();
+        return ResponseEntity.ok(body);
+    }
 }
